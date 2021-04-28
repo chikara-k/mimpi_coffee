daa711b3c31bd18872c1d10200ee554cce705323
chikara-k <wabisabitea.c@gmail.com>
2021-04-28 14:46:01 +0000

    devise_created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'homes#top'
+
+  devise_for :admins, controllers: {
+	  sessions: 'admins/sessions'
+	}
+
+	devise_scope :admins do
+		get 'admins/sign_in' => 'admins/sessions#create'
+		get 'admins/sign_out' => 'admins/sessions#destroy'
+	end
+
+	devise_for :customers, controllers: {
+  	sessions: 'customers/sessions',
+  	passwords: 'customers/passwords',
+  	registrations: 'customers/registrations'
+	}
+
+	devise_scope :customers do
+		get 'customers/sign_in' => 'customers/sessions#create'
+		get 'customers/sign_out' => 'customers/sessions#destroy'
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Admin.find_or_create_by(id: 1) do |admin|
+# admin.email = "admin@admin.com"
+# admin.password = "adminadmin"
+# end
