ef6027791e0e10b181beab916451e01381d5acab
chikara-k <wabisabitea.c@gmail.com>
2021-04-30 03:30:02 +0000

    creation_of_header_and_footer

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,19 @@
+class Admin::ItemsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,33 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-
-  namespace :admin do
-  	root to: 'homes#top'
-  end
-
+# 管理者側
   devise_for :admins, controllers: {
   	sessions: 'admins/sessions'
   }
-
+  
   devise_scope :admins do
-  	get 'admins/sign_in' => 'admins/sessions#create'
+  get 'admins/sign_in' => 'admins/sessions#create'
 	get 'admins/sign_out' => 'admins/sessions#destroy'
   end
 
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  end
+  
+
+# ユーザー側
   devise_for :customers, controllers: {
   	sessions: 'customers/sessions',
   	passwords: 'customers/passwords',
   	registrations: 'customers/registrations'
   }
-
+  
   devise_scope :customers do
 	get 'customers/sign_in' => 'customers/sessions#create'
 	get 'customers/sign_out' => 'customers/sessions#destroy'
   end
+  
+  
+  root to: 'homes#top'
 end
