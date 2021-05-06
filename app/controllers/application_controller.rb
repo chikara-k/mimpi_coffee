cc6936f213d5bd1a574ee1abcd5daec50512e997
chikara-k <wabisabitea.c@gmail.com>
2021-05-06 14:48:44 +0000

    layout_adjustment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
       when Admin
-        admins_root_path
+        admin_root_path
       when Customer
         #? とりあえずroot_path 変更=> customer_path(current_customer.id)
         root_path
