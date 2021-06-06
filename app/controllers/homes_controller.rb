72994de4f18ead0ce55c67802aebc9aa329d4a72
chikara-k <wabisabitea.c@gmail.com>
2021-06-06 13:13:56 +0000

    fix: top&turbolinks

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,8 @@ class HomesController < ApplicationController
   def top
     items = Item.includes(:favorited_customers).sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
     @items = items.first(10)
+
+    @new_items = Item.last(8)
   end
 
   def about
