f6166c503f4dc4ce800ff4efb0f6acbb19a06373
chikara-k <wabisabitea.c@gmail.com>
2021-06-08 13:29:44 +0000

    fix: top_newitem

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
     items = Item.where(is_sales: true).includes(:favorited_customers).sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
     @items = items.first(10)
 
-    @new_items = Item.last(8)
+    @new_items = Item.where(is_sales: true).last(8)
   end
 
   def about
